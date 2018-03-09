33ba6e1b05ed80ecf94bf7599de2b957e177d309
Malicioso <jaimelopezsanchz@gmail.com>
2018-03-09 05:40:10 +0100

    version mejorada

--- servidor.c
+++ servidor.c
@@ -23,11 +23,18 @@
 #define DEFAULT_DIRECTORY_INDEX             "index.html"
 #define DEFAULT_MAX_CLIENTS                 15
 
+#define HTTP_BAD_REQUEST  					"HTTP/1.1 400 BAD REQUEST\r\nContent-Length: 206\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n<html>\n<head>\n<title>400 Bad Request. Que lo sepas</title>\n</head>\n<body>\n<h1>Bad Request</h1>\n The requested URL, file type or operation is not allowed on this simple static file webserver.\n</body>\n</html>\n"
 #define REQUEST  "Hola a todos\n"
 
 #define MAX_REQUEST_SIZE                    4048
 #define MAX_PATH_SIZE                       256
 #define BUFSIZE                             8096
+
+#ifdef DEBUG
+#define log(fmt,args...) fprintf(stderr,fmt "\n",##args)
+#else
+#define log(fmt,args...) 
+#endif
 
 
 int AtenderProtocoloHTTP(int sd)
@@ -35,13 +42,22 @@ int AtenderProtocoloHTTP(int sd)
     char buffer[MAX_REQUEST_SIZE] = "\0";
     long bLeidos = 0;
     int  result;
+    
+    log("Atendiendo la solicitud HTTP...");
 
     // HTTP REQUEST: leemos la cabecera (debe ser siempre menor que MAX_REQUEST_SIZE)
     bLeidos = read(sd, buffer, sizeof(buffer)-1);
+    if(bLeidos < 0) {
+		fprintf(stderr, "ERROR: [%s] error leyendo del socket\n",strerror(errno));
+		return -1;
+	}
+	
     buffer[bLeidos] = '\0';
-
+	log("Recibidos %ld bytes\n%s",bLeidos,buffer);
+	
     // HTTP RESPONSE: respondemos con un mensaje
-    result = write(sd, REQUEST, strlen(REQUEST));
+    log("Enviando request (%ld bytes):\n%s",strlen(HTTP_BAD_REQUEST),HTTP_BAD_REQUEST);
+    result = write(sd, HTTP_BAD_REQUEST, strlen(HTTP_BAD_REQUEST));
 
     return result;
 }   /* de AtenderProtocoloHTTP */
@@ -54,17 +70,34 @@ int ObtenerSocket(int puerto)
     int                 error;
 
     // SOCKET: Abrimos un socket donde esperar peticiones de servicio
+    log("SOCKET: abriendo un socket...");
     sfdServidor = socket(AF_INET, SOCK_STREAM, 0);
+    if(sfdServidor==-1) {
+		fprintf(stderr,"ERROR [%s] abriendo el socket\n",strerror(errno));
+		return -1;
+	}
 
     // BIND: Asociamos el puerto en el que deseamos esperar con el socket obtenido
+    log("BIND: asociando el socket con el puerto de escucha...");
     addr.sin_family=AF_INET;
     addr.sin_addr.s_addr=INADDR_ANY;
     addr.sin_port=htons((u_short)puerto);
 
     error = bind(sfdServidor, (struct sockaddr *)&addr, sizeof(addr));
+    if(error < 0){
+		fprintf(stderr,"ERROR [%s] en bind\n",strerror(errno));
+		close(sfdServidor);
+		return -1;
+	}
 
     // LISTEN: Nos ponemos a la escucha en el socket, indicando cuántas atendemos a la vez
+    log("LISTEN: indicamos que atienda hasta %d conexiones simultaneas",DEFAULT_MAX_CLIENTS);
     error = listen(sfdServidor, DEFAULT_MAX_CLIENTS);
+    if(error < 0){
+		fprintf(stderr,"ERROR [%s] en listen\n",strerror(errno));
+		close(sfdServidor);
+		return -1;
+	}
 
     return sfdServidor;
 }   // de ObtenerSocket
@@ -74,40 +107,75 @@ int ConectarConCliente(int sfdServidor)
 {
     int                 sfdCliente = -1;    // socket del hijo. Atiende cada nueva petición
     struct sockaddr_in  cliente;
-           socklen_t    tamCliente = sizeof (cliente);
+           socklen_t    tamCliente = sizeof (cliente);       
+    struct hostent *host;
 
     // ACCEPT: Esperamos hasta que llegue una comunicación, para la que nos dan un nuevo socket
+    log("ACCEPT: esperando nueva conexion...");
     sfdCliente = accept(sfdServidor, (struct sockaddr *)&cliente, &tamCliente);
-
+	
+	log("Comprobando conexion entrante...(resolucion inversa del DNS)");
+	host = gethostbyaddr((char *)&cliente.sin_addr, sizeof(cliente.sin_addr),cliente.sin_family);
+	log("Conexion aceptada a [%s] %s:%u",host->h_name,inet_ntoa(cliente.sin_addr),cliente.sin_port);
+	
     return sfdCliente;
 }   // de ConectarConCliente
 
-
-int main()
+void LanzarServicio(int puerto)
 {
-    int sfdServidor = -1;   // socket del padre. Escucha nuevas peticiones
+	int sfdServidor = -1;   // socket del padre. Escucha nuevas peticiones
     int sfdCliente = -1;    // socket del hijo. Atiende cada nueva petición
     int result;
 
-    sfdServidor = ObtenerSocket(DEFAULT_PORT);
+    sfdServidor = ObtenerSocket(puerto);
+    
+    if(sfdServidor==-1) {
+		fprintf(stderr, "ERROR: no se ha podido establecer al conexion\n");
+		exit(-1);
+	}
 
     while (1) {
         sfdCliente = ConectarConCliente(sfdServidor);
-
+        
+		if(sfdCliente==-1) {
+			fprintf(stderr, "ERROR: no se ha podido conectar con el cliente\n");
+			exit(-1);
+		}
+		
         switch (fork()) {
             case -1:    // Error
+				fprintf(stderr,"ERROR: [%s] ejecutando fork\n",strerror(errno));
                 close(sfdCliente);
                 close(sfdServidor);
                 exit (-1);
 
             case  0:    // El hijo atiende la nueva solicitud
+				log("PROCESO HIJO %d: atendiendo proceso de aplicacion en socket %d",(int)getpid(),sfdCliente);
                 close(sfdServidor);
                 result = AtenderProtocoloHTTP(sfdCliente);      // Atendemos el PROTOCOLO concreto
-                close(sfdCliente);                              
+                close(sfdCliente);     
+                log("PROCESO HIJO %d: terminando proceso",(int)getpid());                         
                 exit (result);
 
             default:    // El padre se queda a la espera de nuevas conexiones
+            log("PROCESO PADRE %d: buscando una nueva conexion ",(int)getpid());
                 close(sfdCliente);
         }
     }   /* de while infinito */
+}
+
+int main()
+{
+	//mostrando informacion de configuracion
+	log("CONFIG: Puerto de escucha: %d",DEFAULT_PORT);
+	log("CONFIG: Max Clientes: %d",DEFAULT_MAX_CLIENTS);
+	log("CONFIG: Recurso por defecto: %s", DEFAULT_DIRECTORY_INDEX);
+	log("CONFIG: Directorio raiz: %s",DEFAULT_ROOT_DIRECTORY);
+	
+	//lanzando el servicio que este en el puerto indicado
+	log("Lanzamos servicio en puerto: %d",DEFAULT_PORT);
+	LanzarServicio(DEFAULT_PORT);
+   
+	//en realidad nunca llegaremos aqui
+	return 1;
 }	/* de main */
